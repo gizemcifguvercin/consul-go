66ec6286cbea539bfdd0b590236f8d22b2683a89
gcifguvercin <Gc060820>
2021-12-05 14:47:28 +0300

    app config changed :fire:

--- watch.go
+++ watch.go
@@ -1,8 +1,6 @@
 package consul_go
 
 import (
-	"encoding/json"
-
 	"github.com/hashicorp/consul/api"
 	"github.com/jasonlvhit/gocron"
 )
@@ -30,14 +28,14 @@ func NewConsulWatcher(c *ConsulConfig) ConsulWatcher {
 	return ConsulWatcher{consulClient: consulClient}
 }
 
-func (w *ConsulWatcher) Watch(appConfig *interface{}) {
-	data := w.consulClient.Read()
-	json.Unmarshal([]byte(data), &appConfig)
+func (w *ConsulWatcher) Watch() (data string) {
+	data = w.consulClient.Read()
+	return data
 }
 
-func (w *ConsulWatcher) StartWatch(appConfig *interface{}) {
+func (w *ConsulWatcher) StartWatch() {
 	go func() {
-		gocron.Every(uint64(w.consulClient.Config.Interval)).Seconds().Do(w.Watch, &appConfig)
+		gocron.Every(uint64(w.consulClient.Config.Interval)).Seconds().Do(w.Watch)
 		<-gocron.Start()
 	}()
 }
